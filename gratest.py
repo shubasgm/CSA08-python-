04686023b58bfcecdf82a480c2c1cc1483aeee6b
shubas <128580083+shubasgm@users.noreply.github.com>
2024-07-04 13:56:13 +0530

    Add files via upload

---
+++ gratest.py
@@ -0,0 +1,11 @@
+# Define the three numbers
+a = 7
+b = 12
+c = 5
+
+# Find the greatest number using max function
+greatest_number = max(a, b, c)
+
+# Print the result
+print("The greatest number is:", greatest_number)
+
